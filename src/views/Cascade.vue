15df0c277d93a612a0926a443c95af5ea926ce81
bizzld <1805960473@qq.com>
2022-06-15 23:59:29 +0800

    first commit

---
+++ src/views/Cascade.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Cascade"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Rate.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Rate"
+}
+</script>
+
+<style scoped>
+
+</style>
